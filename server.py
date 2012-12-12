7c37e94dfea5b9ecba7ef7845d97abee1ce70f92
Thomas Millar <millar.thomas@gmail.com>
2012-12-12 00:23:26 -0500

    Added search

--- server.py
+++ server.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    test = getTweets()
-    return render_template('index.html', name=test)
+    return render_template('index.html')
+
+@app.route("/sentiment/<search>")
+def sentiment(search):
+    results = getTweets(search)
+    return render_template('index.html', tweets=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
